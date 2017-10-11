5dbb688e25d8ff4e1945bb83fa160cdd3f35bee4
tomaszlukasik <tomasz.lukasik@schibsted.pl>
2017-10-11 11:28:18 +0200

    Refactor handling socket events.

---
+++ src/handlers/index.js
@@ -0,0 +1,52 @@
+const defaultOpts = {
+    screenshotFreq: 50
+};
+
+const actions = {
+    resize: async (browser, viewport) => browser.setViewport(viewport)
+};
+
+function handlers(browser, options) {
+    const settings = Object.assign({}, defaultOpts, options);
+    const { screenshotFreq } = settings;
+
+    let timeout;
+    let socket;
+
+    function stopTimeout() {
+        return timeout && clearTimeout(timeout);
+    }
+
+    async function sendScreenshot() {
+        if (socket) {
+            const buffer = await browser.screenshot();
+            socket.volatile.emit('screen', { image: buffer.toString('base64') });
+            console.log(`Screenshot sent. Timestamp: ${new Date().getTime()}`);
+        }
+        timeout = setTimeout(() => sendScreenshot(), screenshotFreq);
+    }
+
+    async function handleAction({ type, params }) {
+        stopTimeout();
+        console.log(`Received action ${type} with params ${JSON.stringify(params)}`);
+        if (actions[type]) {
+            await actions[type](params);
+        }
+        await sendScreenshot();
+    }
+
+    return {
+        setSocket: sock => (socket = sock),
+        sendScreenshot,
+        handlers: {
+            action: handleAction,
+            disconnect: async () => {
+                console.log('Disconnecting');
+                stopTimeout();
+                return browser.close();
+            }
+        }
+    };
+}
+
+module.exports = handlers;
