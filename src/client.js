53520b5c6f44b6177cb443ed642442b4f4cf68a1
Karol Maj <karol.maj@schibsted.pl>
2017-10-11 14:46:45 +0200

    Name clicked button in the mouse click event

--- src/client.js
+++ src/client.js
@@ -4,6 +4,10 @@ const throttle = require('lodash.throttle');
 const PresentationLayer = require('./client/presentation');
 const TransportLayer = require('./client/transport');
 
+const Mapper = {
+    button: (button) => button === 0 ? 'left' : button === 1 ? 'middle' : 'right'
+};
+
 (function () {
     console.info('Welcome to the Hack Day 2017!');
     console.info('Chromeer - Remote Browser with an isolation layer');
@@ -17,18 +21,19 @@ const TransportLayer = require('./client/transport');
         presentation.resize({ width, height });
     };
 
+
     // proxy client events
     window.addEventListener('resize', throttle(updateCanvasDimension, 500), false);
 
     window.addEventListener('mousemove', throttle((event) => transport.emit('mousemove', {
-        x: event.clientX,
-        y: event.clientY
+        x: event.pageX,
+        y: event.pageY
     }), 500), false);
 
     window.addEventListener('click', (event) => transport.emit('click', {
         x: event.clientX,
         y: event.clientY,
-        button: event.button
+        button: Mapper.button(event.button)
     }), false);
 
     window.addEventListener('dblclick', (event) => transport.emit('dblclick', {
